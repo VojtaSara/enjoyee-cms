fc5e9f2facbd60cb75ff03e9eccdfe9ded8c82e9
VojtaSara <vojta.sara@gmail.com>
2022-11-17 16:28:04 +0100

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -3,11 +3,11 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'aws-s3',
         providerOptions: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-          region: env('AWS_REGION'),
+          accessKeyId: env('AKIA5SWMQNJYBIVO5AWI'),
+          secretAccessKey: env('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'),
+          region: env('eu-central-1'),
           params: {
-              Bucket: env('AWS_BUCKET_NAME'),
+              Bucket: env('enjoye-strapi-images'),
           },
         },
         // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
